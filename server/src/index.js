cbe83823697310176d06d12fd5e5160bccad961a
Mario <pyrros2000@gmail.com>
2022-08-11 15:05:20 -0700

    Created and started apollo server into sandbox

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({typeDefs});
+
+server.listen().then(() => {
+    console.log(`
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at http://localhost:4000
+    `)
+});

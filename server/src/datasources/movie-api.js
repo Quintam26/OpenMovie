e265d422a729caeb70e9a02fa53cbf0bcba42587
Mario <pyrros2000@gmail.com>
2022-09-01 20:09:27 -0700

    Added revies api endpoint and functions

--- server/src/datasources/movie-api.js
+++ server/src/datasources/movie-api.js
@@ -2,6 +2,7 @@ const { RESTDataSource } = require('apollo-datasource-rest');
 require("dotenv").config();
 
 const ApiKey = process.env.API_KEY;
+const ReviewApiKey = process.env.REVIEW_API_KEY;
 
 class MovieAPI extends RESTDataSource {
     constructor() {
@@ -18,4 +19,18 @@ class MovieAPI extends RESTDataSource {
     }
 }
 
-module.exports = MovieAPI;
+class ReviewAPI extends RESTDataSource {
+    constructor() {
+        super()
+        this.baseURL = "https://api.nytimes.com/svc/movies/v2";
+    }
+
+    getReviews() {
+        return this.get(`reviews/search.json?api-key=${ReviewApiKey}`);
+    }
+    getReviewsByMovie(name) {
+        return this.get(`reviews/search.json?query=${name}&api-key=${ReviewApiKey}`);
+    }
+}
+
+module.exports = MovieAPI, ReviewAPI;
